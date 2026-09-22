2b25eeec16278bb68e4ef9755c1f7a72c9f963d2
razco1212 <raztektok@gmail.com>
2026-09-22 23:57:44 +0300

    Remove unused import statement in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,7 @@ import frc.robot.Constants.OperatorConstants;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 
 
 public class RobotContainer {
